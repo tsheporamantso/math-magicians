9a8efed82c7e3dea8248d0a29384f7056ce9db4f
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-27 20:32:09 +0200

    Removed h2 tag and fragment

--- src/components/MyCalculator.js
+++ src/components/MyCalculator.js
@@ -12,49 +12,44 @@ function Calculator() {
   };
 
   return (
-    <>
-      <div className="wrapper">
-        <div className="header-container">
-          <h2>Let&apos;s do some math!</h2>
-        </div>
-        <div className="container">
-          <div className="child__result">
-            {total}
-            {operation}
-            {next}
-          </div>
-          <div className="child__container">
-            <button type="button" className="button_key" onClick={calculateResult}>AC</button>
-            <button type="button" className="button_key" onClick={calculateResult}>+/-</button>
-            <button type="button" className="button_key" onClick={calculateResult}>%</button>
-            <button type="button" className="button_operator" onClick={calculateResult}>÷</button>
-          </div>
-          <div className="child__container">
-            <button type="button" className="button_key" onClick={calculateResult}>7</button>
-            <button type="button" className="button_key" onClick={calculateResult}>8</button>
-            <button type="button" className="button_key" onClick={calculateResult}>9</button>
-            <button type="button" className="button_operator" onClick={calculateResult}>x</button>
-          </div>
-          <div className="child__container">
-            <button type="button" className="button_key" onClick={calculateResult}>4</button>
-            <button type="button" className="button_key" onClick={calculateResult}>5</button>
-            <button type="button" className="button_key" onClick={calculateResult}>6</button>
-            <button type="button" className="button_operator" onClick={calculateResult}>-</button>
-          </div>
-          <div className="child__container">
-            <button type="button" className="button_key" onClick={calculateResult}>1</button>
-            <button type="button" className="button_key" onClick={calculateResult}>2</button>
-            <button type="button" className="button_key" onClick={calculateResult}>3</button>
-            <button type="button" className="button_operator" onClick={calculateResult}>+</button>
-          </div>
-          <div className="child__container">
-            <button type="button" className="button_key zero" onClick={calculateResult}>0</button>
-            <button type="button" className="button_key" onClick={calculateResult}>.</button>
-            <button type="button" className="button_key equal" onClick={calculateResult}>=</button>
-          </div>
-        </div>
+
+    <div className="container">
+      <div className="child__result">
+        {total}
+        {operation}
+        {next}
       </div>
-    </>
+      <div className="child__container">
+        <button type="button" className="button_key" onClick={calculateResult}>AC</button>
+        <button type="button" className="button_key" onClick={calculateResult}>+/-</button>
+        <button type="button" className="button_key" onClick={calculateResult}>%</button>
+        <button type="button" className="button_operator" onClick={calculateResult}>÷</button>
+      </div>
+      <div className="child__container">
+        <button type="button" className="button_key" onClick={calculateResult}>7</button>
+        <button type="button" className="button_key" onClick={calculateResult}>8</button>
+        <button type="button" className="button_key" onClick={calculateResult}>9</button>
+        <button type="button" className="button_operator" onClick={calculateResult}>x</button>
+      </div>
+      <div className="child__container">
+        <button type="button" className="button_key" onClick={calculateResult}>4</button>
+        <button type="button" className="button_key" onClick={calculateResult}>5</button>
+        <button type="button" className="button_key" onClick={calculateResult}>6</button>
+        <button type="button" className="button_operator" onClick={calculateResult}>-</button>
+      </div>
+      <div className="child__container">
+        <button type="button" className="button_key" onClick={calculateResult}>1</button>
+        <button type="button" className="button_key" onClick={calculateResult}>2</button>
+        <button type="button" className="button_key" onClick={calculateResult}>3</button>
+        <button type="button" className="button_operator" onClick={calculateResult}>+</button>
+      </div>
+      <div className="child__container">
+        <button type="button" className="button_key zero" onClick={calculateResult}>0</button>
+        <button type="button" className="button_key" onClick={calculateResult}>.</button>
+        <button type="button" className="button_key equal" onClick={calculateResult}>=</button>
+      </div>
+    </div>
+
   );
 }
 
