981d800a8369b64f29216f1453886f9fc5f84dbe
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-27 19:34:38 +0200

    Rectify quote route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Calculator from './components/MyCalculator';
 import './App.css';
 import Home from './components/Home';
 import Navigation from './components/Navbar';
+import Quote from './components/Quote';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Calculator />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/container" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
       </Routes>
     </div>
   );
