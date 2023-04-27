1dcc253fd6fe99dd0295d20f41f1072637c3532e
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-27 14:47:08 +0200

    Import navigation component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/MyCalculator';
 import './App.css';
 import Home from './components/Home';
+import Navigation from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <Calculator />
       <Routes>
         <Route path="/" element={<Home />} />
