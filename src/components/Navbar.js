598b90aa356a6949c407a301c0078f376a682755
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-27 14:48:10 +0200

    Add nav links for calculator and qoute

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,16 @@ function Navigation() {
             Home
           </Link>
         </li>
+        <li>
+          <Link to="container" className="links">
+            Calculator
+          </Link>
+        </li>
+        <li>
+          <Link to="qoute" className="links">
+            Quote
+          </Link>
+        </li>
       </ul>
     </nav>
   );
