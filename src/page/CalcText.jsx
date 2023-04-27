0aefc5cfd78d7bee61a73d10608c2e8c0851a5cd
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-27 20:30:19 +0200

    Add page component for calculator text

---
+++ src/page/CalcText.jsx
@@ -0,0 +1,14 @@
+import Calculator from '../components/MyCalculator';
+
+function Calc() {
+  return (
+    <>
+      <div className="calc">
+        <h2>Let&apos;s do some Math!</h2>
+        <Calculator />
+      </div>
+    </>
+  );
+}
+
+export default Calc;
