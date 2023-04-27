fd4c2e8510d0d151faf0e9127683e18cfbe33154
tsheporamantso <tshepo.ramantso@outlook.com>
2023-04-27 14:02:58 +0200

    Import home component and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/MyCalculator';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Calculator />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
