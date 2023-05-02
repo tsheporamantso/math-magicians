b103e065df2f6a80aaf91f5124429fba296488a9
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-02 15:03:50 +0200

    Change App.scss path to start from styles folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import './App.scss';
+import './styles/App.scss';
 import Home from './page/Home';
 import Navigation from './page/Navbar';
 import Quote from './page/Quote';
--- src/page/Navbar.jsx
+++ src/page/Navbar.jsx
@@ -1,4 +1,4 @@
-import '../App.scss';
+import '../styles/App.scss';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
