b5212eece89b16ed90edeef1fa0c2dcc74b5d27e
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-02 13:08:23 +0200

    Move and Add React inline style from App.css to App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import CalcText from './page/CalcText';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ width: '80vw', margin: '30px auto' }}>
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
