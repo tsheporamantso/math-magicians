f9b749aedc62a6dbc713f9a8f05e12986eb4737b
tsheporamantso <tshepo.ramantso@outlook.com>
2023-05-03 19:36:58 +0200

    Correct test function import Browser Router and wrapped it around Navigation

--- src/tests/Navbar.test.js
+++ src/tests/Navbar.test.js
@@ -1,9 +1,15 @@
-import { render } from '@testing-library/react';
-import Navbar from '../page/Navbar';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
+import Navigation from '../page/Navbar';
 
-describe('Navbar', () => {
+describe('Navigation', () => {
   test('should match Navbar snapshot', () => {
-    const tree = render(<Navbar />);
+    const tree = renderer.create(
+      <BrowserRouter>
+        <Navigation />
+        ,
+      </BrowserRouter>,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
